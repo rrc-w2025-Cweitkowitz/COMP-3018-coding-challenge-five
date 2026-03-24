f11d509b40a837d14d674e65a708078cb98e7340
Abdelhamid Oughanem <aoughanem@academic.rrc.ca>
2026-03-24 17:22:46 -0500

    added test

---
+++ test/resourceService.test.ts
@@ -0,0 +1,24 @@
+import { getAllRessources, getRessourcesById } from "../services/resourceService";
+import { Resources } from "../models/resourceModels";
+
+describe("Resource Service", () => {
+  describe("getAllRessources", () => {
+    it("should return all resources", async () => {
+      const resources: Resources[] = await getAllRessources();
+      expect(resources).toHaveLength(4); 
+      expect(resources[0].title).toBe("Express.js Guide");
+    });
+  });
+
+  describe("getRessourcesById", () => {
+    it("should return the resource with given id", async () => {
+      const resource: Resources = await getRessourcesById("2");
+      expect(resource.title).toBe("TypeScript Basics");
+      expect(resource.id).toBe("2");
+    });
+
+    it("should throw an error if resource is not found", async () => {
+      await expect(getRessourcesById("999")).rejects.toThrow("Resource not found");
+    });
+  });
+});
